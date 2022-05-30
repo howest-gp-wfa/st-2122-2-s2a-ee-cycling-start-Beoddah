3b3232bb7d03350c2e4d159e83d500b68fa494e7
Beoddah <thomas.de.fauw@student.howest.be>
2022-05-30 18:24:12 +0200

    Fetch json data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 "use strict";
 
+//global vars
+let teamsData;
+
+
 window.addEventListener('load',initialize);
 
 
 
 function initialize(){
-
+    loadData();
 }
 
+async function loadData() {
+    fetch("https://howest-gp-wfa.github.io/st-2122-2-s2a-ee-cycling-start-Beoddah/api/data.json")
+        .then(function(resp) {
+            return resp.json();
+        })
+            .then (function(data) {
+                teamsData = data;
+            })
+        .catch(error => console.log(error));
+
+
+}       
